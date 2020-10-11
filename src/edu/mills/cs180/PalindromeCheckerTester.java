8783e94759a63426fca333a6a5a763de8d074d9f
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-10-11 03:49:14 -0700

    Add more tests

--- src/edu/mills/cs180/PalindromeCheckerTester.java
+++ src/edu/mills/cs180/PalindromeCheckerTester.java
@@ -1,7 +1,10 @@
 package edu.mills.cs180;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PalindromeCheckerTester {
 
@@ -10,4 +13,15 @@ class PalindromeCheckerTester {
         assertTrue(PalindromeChecker.isPalindrome(""));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"AMA", "racecar", "x"})
+    void isPailndrome_True_Palindromes(String input) {
+        assertTrue(PalindromeChecker.isPalindrome(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "abc", "spatula"})
+    void isPailndrome_False_Palindromes(String input) {
+        assertFalse(PalindromeChecker.isPalindrome(input));
+    }
 }
